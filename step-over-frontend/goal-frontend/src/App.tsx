f00c9d0beaa5bb1547ec913636f426c020577e0a
Sofia <misofser@gmail.com>
2026-01-02 23:44:36 +0400

    Add initial frontend with Goals List

---
+++ step-over-frontend/goal-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import GoalList from "./GoalList";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>StepOver</h1>
+      <GoalList />
+    </div>
+  );
+}
+
+export default App;
