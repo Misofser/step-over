79d0eebe36b2dd6c1a6fafc44e968dd2c58fea91
Sofia <misofser@gmail.com>
2026-01-12 13:16:50 +0400

    Add auth API functions on frontend

---
+++ step-over-frontend/goal-frontend/src/api/auth.ts
@@ -0,0 +1,37 @@
+import { API_URL } from '../config';
+
+export async function login(username: string, password: string) {
+  const response = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "include",
+    body: JSON.stringify({ username, password })
+  });
+
+  if (!response.ok) {
+    throw new Error("Login failed");
+  }
+
+  return response.json();
+}
+
+export async function getMe() {
+  const res = await fetch(`${API_URL}/auth/me`, {
+    credentials: "include"
+  });
+
+  if (!res.ok) {
+    throw new Error("Not authenticated");
+  }
+
+  return res.json();
+}
+
+export async function logout() {
+   return fetch(`${API_URL}/auth/logout`, {
+    method: "POST",
+    credentials: "include"
+  });
+};
