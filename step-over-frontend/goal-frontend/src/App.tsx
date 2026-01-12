44adeb778df3c495572de7562bb458362e6b13b5
Sofia <misofser@gmail.com>
2026-01-12 13:52:18 +0400

    Add Login page

--- step-over-frontend/goal-frontend/src/App.tsx
+++ step-over-frontend/goal-frontend/src/App.tsx
@@ -1,74 +1,27 @@
-import { useEffect, useState } from "react";
-
 import './App.css'
-import type { Goal } from './api/goals.types';
-import GoalList from './components/GoalList/GoalList';
-import { EditGoalModal } from "./components/EditGoalModal/EditGoalModal";
-import NewGoalForm from './components/NewGoalForm/NewGoalForm';
-import { addGoal as apiAddGoal, fetchGoals, deleteGoal, updateGoal } from "./api/goals";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router"
+import { LoginPage } from './pages/LoginPage/LoginPage'
+import { GoalsPage } from "./pages/GoalsPage"
+import { ProtectedRoute } from "./auth/ProtectedRoute"
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
-  const [goals, setGoals] = useState<Goal[]>([]);
-  const [editingGoalId, setEditingGoalId] = useState<number | null>(null);
-
-  useEffect(() => {
-    fetchGoals().then(setGoals);
-  }, []);
-
-  const addGoal = async (title: string) => {
-    const newGoal = await apiAddGoal(title);
-    setGoals(prev => [...prev, newGoal]);
-  };
-
-  const handleDeleteGoal = async (id: number) => {
-    try {
-      await deleteGoal(id);
-      setGoals(prev => prev.filter(goal => goal.id !== id));
-    } catch (e) {
-      alert("Could not delete goal");
-    }
-  };
-
-  const handleToggleGoal = async (goal: Goal) => {
-    try {
-      await updateGoal(goal.id, {isCompleted: !goal.isCompleted});
-      setGoals(prev =>
-        prev.map(g =>
-          g.id === goal.id ? { ...g, isCompleted: !goal.isCompleted } : g
-        )
-      );
-    } catch (e) {
-      alert("Could not update goal")
-    }
-  };
-
-  const handleSavedGoal = (id: number, newTitle: string) => {
-    setGoals(prev =>
-      prev.map(g =>
-        g.id === id ? { ...g, title: newTitle } : g
-      )
-    );
-  };
-
   return (
-    <div className="app-container">
-      <h1>StepOver</h1>
-      <h2>Goals List</h2>
-      <NewGoalForm onAddGoal={addGoal} />
-      <GoalList
-        goals={goals}
-        onDelete={handleDeleteGoal}
-        onToggle={handleToggleGoal}
-        onEdit={goal => setEditingGoalId(goal.id)}
-      />    
-      {editingGoalId && (
-        <EditGoalModal
-          goalId={editingGoalId}
-          onClose={() => setEditingGoalId(null)}
-          onSave={handleSavedGoal}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/goals"
+          element={
+            <ProtectedRoute>
+              <GoalsPage />
+            </ProtectedRoute>
+          }
         />
-      )}
-    </div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ step-over-frontend/goal-frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,58 @@
+import { useState, useContext } from "react"
+import { Navigate, useNavigate } from "react-router"
+import { AuthContext } from "../../auth/AuthContext";
+import { login as loginApi } from "../../api/auth";
+import "./LoginPage.css";
+
+export function LoginPage() {
+  const { isAuthenticated, loading, login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  if (loading) {
+    return null;
+  }
+
+  if (isAuthenticated) {
+    return <Navigate to="/goals" replace />;
+  }
+
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const data = await loginApi(username, password);
+      login(data.role);
+      navigate("/goals");
+    } catch (err: any) {
+      setError(err.message || "Invalid username or password");
+    }
+  }
+
+  return (
+    <div className="login-page">
+      <form className="login-form" onSubmit={handleLogin}>
+        {error && <div className="error">{error}</div>}
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+           required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
