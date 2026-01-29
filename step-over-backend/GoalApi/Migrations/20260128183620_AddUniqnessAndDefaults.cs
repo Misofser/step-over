6737648be11a3579add7e2c6af9ead60bc7b968f
Sofia <misofser@gmail.com>
2026-01-29 12:49:08 +0400

    Add database uniqueness constraints and default values

---
+++ step-over-backend/GoalApi/Migrations/20260128183620_AddUniqnessAndDefaults.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqnessAndDefaults : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "User",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCompleted",
+                table: "Goals",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldDefaultValue: "User");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCompleted",
+                table: "Goals",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldDefaultValue: false);
+        }
+    }
+}
