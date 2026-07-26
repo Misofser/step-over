e3475e2f1fcaebe8ba511b49aa868da4ce316594
Sofia <misofser@gmail.com>
2026-07-26 15:59:14 +0400

    Allow users to create their own goals on the frontend

--- step-over-frontend/goal-frontend/src/pages/GoalsPage.tsx
+++ step-over-frontend/goal-frontend/src/pages/GoalsPage.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useState, useContext } from "react"
+import { useEffect, useState } from "react"
 import type { Goal, GoalToCreate } from '../api/goals.types'
 import GoalList from '../components/GoalList/GoalList'
 import NewGoalForm from '../components/NewGoalForm/NewGoalForm'
-import { AuthContext } from "../auth/AuthContext"
 import { addGoal as apiAddGoal, fetchGoals } from "../api/goals"
 
 export function GoalsPage() {
   const [goals, setGoals] = useState<Goal[]>([]);
-  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     fetchGoals().then(setGoals);
@@ -22,9 +20,7 @@ export function GoalsPage() {
     <div className="app-container">
       <h1>StepOver</h1>
       <h2>Goals List</h2>
-      {user?.role === "Admin" && (
-        <NewGoalForm onAddGoal={addGoal} />
-      )}
+      <NewGoalForm onAddGoal={addGoal} />
       <GoalList goals={goals} />
     </div>
   );
